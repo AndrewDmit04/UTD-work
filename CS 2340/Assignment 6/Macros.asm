a6ab6e4d018f071cfb5ab4269df470f756503fb5
AndrewDmit04 <144502433+AndrewDmit04@users.noreply.github.com>
2023-12-17 22:58:08 -0600

    Create Macros.asm

---
+++ CS 2340/Assignment 6/Macros.asm
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------------------------------------------------------------------------------------#
+#written by: Andrew Dmitrievsky
+#NET-ID: AVD210006
+#Purpose: Write a program that will be able to encrypt and decrypt files.
+#the encryption algorithm will use a key which is a string, it will read
+#the file and add the ascii value of the key to the charecter read in the file
+#will also create a .enc file where the encryption will be stored
+#decryption will do the same but subtract, will create a file a .txt file 
+#where the decrypted file will be stored
+#-----------------------------------------------------------------------------------------------------------------------------------------------#
+	#macro will print the string inside of the (" ")
+	.macro printString (%String) 		#has one parameter for the string to be printed 
+	.data 					#data section
+temp:	.asciiz	%String 			#fill intilize the string passed in the macro to a adress 
+	.text	
+	li		$v0, SysPrintString	#load the print String function
+	la		$a0,temp 		#load the string intilized
+	syscall 				#print the string 
+	.end_macro				#end the macro
